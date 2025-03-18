b0e2ee9bf7407a335777c6ecf4d5a058f755ad9b
Suharjin <suharjin01@gmail.com>
2025-03-18 20:58:30 +0800

    Setup awal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, serve, Router};
+use tokio::net::TcpListener;
+
+// Setup
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(|| async {"Hello, World!"}));
+    
+    let listener = TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    // menjalankan server
+    serve(listener, app).await.unwrap();
 }
